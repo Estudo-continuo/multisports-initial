40e985ea506137914b42ace6f53d19776b635ede
Fernando Moreti <fernandombolela@gmail.com>
2024-05-13 17:41:34 -0300

    docs: change name os references and add a function broke

--- public/js/futebol/filter.js
+++ public/js/futebol/filter.js
@@ -3,23 +3,31 @@ async function loadFilterOptions() {
     const filter = document.getElementById("jogos_position")
 
     options.json().then(res => {
+
         res.forEach(option => {
+
+            if (option.isFavorite == false) {
+                heart = "grey"
+            } else {
+                heart = "red"
+            }
+
             filter.innerHTML += `
         <section class="jogos">
 
             <div class="jogos_dados">
                 <div class="jogos_dados_club">
-                    <img class="jogos_dados_club_img" src="${option.img1}" alt="${option.name1}">
-                    <p class="jogos_dados_write">${option.name1}</p>
+                    <img class="jogos_dados_club_img" src="${option.imgClubHouse}" alt="${option.nameClubHouse}">
+                    <p class="jogos_dados_write">${option.nameClubHouse}</p>
                 </div>
                 <p class="jogos_dados_vs">vs</p>
                 <div class="jogos_dados_club">
-                    <img class="jogos_dados_club_img" src="${option.img2}" alt="${option.name2} F.C">
-                    <p class="jogos_dados_write">${option.name2}</p>
+                    <img class="jogos_dados_club_img" src="${option.imgClubVisited}" alt="${option.nameClubVisited} F.C">
+                    <p class="jogos_dados_write">${option.nameClubVisited}</p>
                 </div>
                 <p class="jogos_dados_write"><i class="fa-regular fa-clock"></i> ${option.time}</p>
                 <p class="jogos_dados_write"><i class="fa-solid fa-location-dot"></i> ${option.local}</p>
-                <button class="icon-heart-${option.heart}"><i class="fa-solid fa-heart"></i></button>
+                <button onClick="addFavorite(value)" value="${option.reference}" class="icon-heart-${heart}"><i class="fa-solid fa-heart"></i></button>
                 <a class="jogos_dados_viewmore" href="#">View more <i class="fa-solid fa-arrow-right"></i></a>
             </div>
 
@@ -28,6 +36,22 @@ async function loadFilterOptions() {
         })
     })
 }
+
+// async function addFavorite(value) {
+//     const referencia = await fetch("../../data/futebol/filter.json")
+
+//     referencia.json().then(resposta => {
+//         console.log(resposta[value].isFavorite)
+//         let alterar = resposta[value].isFavorite
+//         if (alterar == true) {
+//             alterar = false
+//         } else {
+//             alterar = true 
+//         }
+//     })
+// }
+
+
 
 function main() {
     loadFilterOptions()
