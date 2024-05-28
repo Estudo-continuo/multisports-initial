3d9a8f571a816df417fb9e8b8654948c3b6df3e6
Fernando Moreti <fernandombolela@gmail.com>
2024-05-28 10:01:45 -0300

    docs: add a new div in html

--- public/js/futebol/filter.js
+++ public/js/futebol/filter.js
@@ -17,18 +17,26 @@ async function loadFilterOptions() {
 
             <div class="jogos_dados">
                 <div class="jogos_dados_club">
-                    <img class="jogos_dados_club_img" src="${option.home.img}" alt="${option.home.name}">
-                    <p class="jogos_dados_write">${option.home.name}</p>
+                    <div class="position_size">
+                        <img class="jogos_dados_club_img" src="${option.home.img}" alt="${option.home.name}">
+                        <p class="jogos_dados_write">${option.home.name}</p>
+                    </div>    
                 </div>
-                <p class="jogos_dados_vs">vs</p>
+                    <p class="jogos_dados_vs">vs</p>
                 <div class="jogos_dados_club">
-                    <img class="jogos_dados_club_img" src="${option.away.img}" alt="${option.away.name} F.C">
-                    <p class="jogos_dados_write">${option.away.name}</p>
+                    <div class="position_size">
+                        <img class="jogos_dados_club_img" src="${option.away.img}" alt="${option.away.name} F.C">
+                        <p class="jogos_dados_write">${option.away.name}</p>
+                    </div>        
                 </div>
-                <p class="jogos_dados_write"><i class="fa-regular fa-clock"></i> ${option.time}</p>
-                <a href="${option.linkLocal}" class="jogos_dados_write"><i class="fa-solid fa-location-dot"></i> ${option.local}</a>
-                <button onClick="addFavorite(value)" value="${option.reference}" class="icon-heart-${heart}"><i class="fa-solid fa-heart"></i></button>
-                <a class="jogos_dados_viewmore" href="#">View more <i class="fa-solid fa-arrow-right"></i></a>
+                    <div class="position_size">
+                        <p class="jogos_dados_write"><i class="fa-regular fa-clock"></i> ${option.time}</p>
+                        <a href="${option.linkLocal}" class="jogos_dados_write"><i class="fa-solid fa-location-dot"></i> ${option.local}</a>
+                    </div>
+                    <div class="position_size">
+                        <button onClick="addFavorite(value)" value="${option.reference}" class="icon-heart-${heart}"><i class="fa-solid fa-heart"></i></button>
+                        <a class="jogos_dados_viewmore" href="#">View more <i class="fa-solid fa-arrow-right"></i></a>
+                    </div>            
             </div>
 
         </section>
